c3492cc089367372a80f77b254df77bf42748d39
alhans2708 <alhans2708@gmail.com>
2024-04-20 16:15:36 +0700

    [INIT] Initial Porject MVC .NET 7.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace port_inventory.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
